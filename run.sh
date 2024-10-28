64033eef4583152c49e6f3e0e997aa8256d7a691
_int_me <intmex@qq.com>
2024-10-28 22:54:23 +0800

    add AffectNet

---
+++ run.sh
@@ -0,0 +1,19 @@
+python main_moco_aff.py
+-a
+resnet50
+--lr
+0.015
+--batch-size
+128
+--world-size
+1
+--rank
+0
+--workers
+16
+--mlp
+--moco-t
+0.2
+--aug-plus
+--cos
+C:/koe/DataCenter/AffectNet
